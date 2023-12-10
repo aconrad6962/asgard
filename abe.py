75ede5ecfc484d214122f42adfda28fe949a0bbb
aconrad6962 <53485665+aconrad6962@users.noreply.github.com>
2023-12-10 11:04:12 -1000

    Basic scripted prototype
    Javascript should not need to change much from here, bu the python backend (abe.py) needs better stand-alone simulation and then the connection to the TBD AO (Mongo?) interface.

--- abe.py
+++ abe.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 #
 #  ABE - Asgard Back End
 #
@@ -9,19 +8,47 @@
 #  Revision History:
 #    10-Oct-2021    ARC    Original 
 
-from lbt.dms.zwgd.core import Producer
+
+from lbt.dms.zwgd.core import AbstractProducer, DataProduct
 import time
 
 name = 'demo1'
 
-pd = Producer(name)
 
-while True:
-    msg = {'rname': 'row1','inprog': 0,'timerem': 0,'cmplt': 1,'fault': 0}
-    print(msg)
-    pd.push_data_json(msg)
-    msg = pd.check_inbox()
-    if msg: print(f'inbox: {msg}')
-      time.sleep(1)
+class Demo1(AbstractProducer):
+    num = 10
+    odMin = -3
+    rnum = 1
 
+    def on_poll(self):
+        self.num -= 1
+        if self.num < self.odMin:
+            t  = "Complete"
+            gc = "lime"
+            yc = "gray"
+            rc = "gray"
+            self.num = 10
+        else:
+          if (self.num > 0 ):
+            t  = "Time Remaining  " + str(self.num)
+            gc = "gray"
+            yc = "yellow"
+            rc = "gray"
+          else:
+            t = "OVERDUE  " + str(self.num)
+            gc = "gray"
+            yc = "gray"
+            rc = "red"
+        mydata = {'row': self.rnum, 'gc': gc, 'yc': yc, 'rc': rc, 'text': t }
+        if self.num == 10:
+          self.rnum += 1
+          if self.rnum > 7:
+            self.rnum = 1
+        return DataProduct(mydata)
+
+    def on_subscribe(self, msg=None):
+        return DataProduct({'message': 'CONNECTED', 'time': time.time()})
+
+pd = Demo1(name)
+pd.mainloop()
 
